5437c1be30d57e6dbf1a72d466b73bd6e45ddb63
Jimmy Li <jimlovebao@gmail.com>
2024-12-26 07:37:52 -0500

    done with the project with search function and statstical display function

--- health_analysis.js
+++ health_analysis.js
@@ -1,6 +1,6 @@
 const addPatientButton = document.getElementById("addPatient");
 const report = document.getElementById("report");
-const btnSearch = document.getElementById('btnSearch');
+const btnSearch = document.getElementById("btnSearch");
 const patients = [];
 
 function addPatient() {
@@ -22,6 +22,40 @@ function resetForm() {
     document.getElementById("age");
     document.getElementById("condition").value = "";
 }
+
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch("health_analysis.json")
+      .then(response => response.json())
+      .then(data => {
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+            const symptoms = condition.symptoms.join(', ');
+            const prevention = condition.prevention.join(', ');
+            const treatment = condition.treatment;
+
+            resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+            resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+            resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+            resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+            resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;            
+        } else {
+            resultDiv.innerHTML = 'Condition not found.';
+        }
+      })
+      .catch(error => {
+        console.error('Error', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+}
+
+btnSearch.addEventListener("click", searchCondition);
+
 
 function generateReport() {
     const numPatients = patients.length;
@@ -64,4 +98,3 @@ function generateReport() {
 }
 
 addPatientButton.addEventListener("click", addPatient);
-
